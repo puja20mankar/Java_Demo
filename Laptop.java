5bffb989d6f4a2ee0a20d47f6d351a423bb52e6c
puja20mankar <117885657+puja20mankar@users.noreply.github.com>
2022-11-15 22:24:01 +0530

    First Program to Commit

---
+++ Laptop.java
@@ -0,0 +1,16 @@
+public class Laptop {
+    String name;
+    String color;
+    int price;
+        public void startLaptop(){
+            System.out.println(name+ "Laptop ON");
+        }
+        public void stopLaptop{
+        System.out.println(color+ "Laptop OFF");
+    }
+    public void LaptopDetails(){
+        System.out.println("Color of Laptop" +color);
+        System.out.println("Name of Laptop" +name);
+        System.out.println("Price of Laptop" +price);
+    }
+}
